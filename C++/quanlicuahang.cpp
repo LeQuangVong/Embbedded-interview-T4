a2ab9530ea92ffdb04a6f8c24554437e0a8d21ba
Le Quang Thuan <thuanlq@bohodecor.vn>
2023-06-02 19:12:58 +0700

    first commit

--- C++/quanlicuahang.cpp
+++ C++/quanlicuahang.cpp
@@ -1,26 +1,74 @@
 #include<iostream>
 #include<string>
+#include<vector>
+using namespace std;
 
-class quanli {
-    public:
-        quanli();
-        int themDoUong();
-        void suaDoUong();
-        void xoaDoUong();
-        void danhSachDoUong();
-}
-
-class doUong {
+class Drink {
     private:
         string name;
-        int gia;
-        int soluong;
+        double price;
     public:
-        doUong(string _name, int _gia, int _soluong);
-        void setName(string name);
-        int getName();
-        void setGia(int gia);
-        int getGia();
-        void setSoluong(int soluong);
-        int getSoluong();
+        Drink(string name, double price);
+        string getName();
+        void setPrice(double price);
+        double getPrice();
+
+};
+
+string Drink::getName(){
+    return Drink::name;
+}
+
+double Drink::getPrice(){
+    return Drink::price;
+}
+
+
+class Manager {
+    private:
+        vector<Drink> DatabaseDrink;
+        int quantityTable();
+        void addDrink();
+        void fixDrink();
+        void delDrink();
+        void listDrinK();
+    public:
+        Manager();
+        int getQuantityTable();
+        vector<Drink> getDatabaseDrink;
+};
+
+typedef struct {
+    Drink name;
+    int quantity;
+}quantityDrink;
+
+typedef struct {
+    int numberTable;
+    bool status;
+    vector<quantityDrink> DatabaseQuatityDrink;
+}table;
+
+class Staff {
+    private:
+        vector<table> DatabaseTable;
+        vector<Drink> DatabaseDrink;
+    public:
+        Staff(vector<table> DatabaseTable, int quantityTable);
+};
+
+Staff::Staff(vector<table> DatabaseTable, int quantityTable){
+    for(Drink drink : DatabaseDrink){
+        DatabaseDrink.push_back(drink);
+    }
+
+    for(int i = 1; i <= quantityTable;i++){
+        table table;
+        table.status = 0;
+        table.numberTable = i;
+        DatabaseTable.push_back(table);
+    }
+}
+int main(){
+    return 0;
 }
